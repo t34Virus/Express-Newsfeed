aba1bcc8c52fdc4d8176f91c1b6dcf37a7d3c245
Teresa Nededog <tnededog@my.hpu.edu>
2015-03-05 15:49:47 -1000

    Initiated method-override, mongoose, express, jade

---
+++ app.js
@@ -0,0 +1,98 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+var methodOverride = require('method-override');
+
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/express-newsfeed_test');
+var Schema = mongoose.Schema;
+
+var newsfeed = new Schema({
+  author : String,
+  title : String,
+  body : String,
+  created : Date
+});
+
+var Newsfeed = mongoose.model('Newsfeed', newsfeed);
+
+// serves static assets
+app.use(express.static(__dirname + '/public'));
+app.use(methodOverride('_method'));
+// parse application/x-www-form-urlencoded 
+app.use(bodyParser.urlencoded({ extended: false }));
+// parse application/json 
+app.use(bodyParser.json());
+app.set('view engine', 'jade');
+
+app.get('/', function (req, res) {
+  Newsfeed.find(function(err, newsfeedsFromDB){
+    if (err) throw err;
+    res.render('index', {newsfeeds: newsfeedsFromDB });  
+  });
+});
+app.get('/newsfeeds/:id', function (req, res) {
+  Newsfeed.findOne({_id:req.params.id},
+    function(err, news){
+    if (err) throw err;
+    res.render('edit_newsfeed', {newsfeed: newsfeed });  
+  });
+});
+app.get('/new_newsfeed', function (req, res) {
+  res.render('new_newsfeed');  
+});
+//new newsfeed
+app.post('/newsfeeds', function (req, res) {
+  var newsfeed = new Newsfeed(
+  {
+    author : req.body.author,
+    title : req.body.title,
+    body : req.body.body,
+    created : new Date()
+  });
+  newsfeed.save(function(err){
+    if (err) throw err;
+    res.redirect("/");
+  });   
+});
+//edit newsfeed
+app.put('/newsfeeds/:id', function (req, res) {
+  Newsfeed.update({_id:req.params.id},
+    { author: req.body.author,
+      title: req.body.title,
+      body: req.body.body,
+    }, function (err, newfeed){
+    res.redirect('/');  
+  });
+});
+
+app.get('/newsfeeds', function(req,res){
+  Newsfeed.find(function(err, newsfeeds){
+    res.render('list', {newsfeeds: newsfeeds});
+  });
+});
+
+//delete newsfeed
+app.delete('/newsfeeds/:id', function (req, res) {
+  Newsfeed.remove({_id:req.params.id},
+    function(err, newsfeed) {
+  res.redirect('/');  
+  });
+});
+app.get('/new_newsfeed', function (req, res) {
+  res.render('new_newsfeed');  
+});
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function (callback) {
+  // yay!
+});
+
+var server = app.listen(3000, function () {
+
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+});
